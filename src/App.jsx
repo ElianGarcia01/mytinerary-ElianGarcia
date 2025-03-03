9d20a277ad9ad2d078ec841db5e3bd3f5a4d0642
Elian Garcia <telljua90@gmail.com>
2025-03-03 06:58:25 -0600

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import HomeLayout from './layouts/HomeLayout'
-import CitiesLayout from "./layouts/CitiesLayout";
 import Home from './pages/Home'
 import Cities from './pages/Cities'
-import NotFound from './pages/NotFound';
+import NotFound from './pages/NotFound'
 
 function App() {
 
@@ -23,13 +22,7 @@ function App() {
         },
         {
           path: "cities",
-          element: <CitiesLayout />,
-          children: [
-            {
-              path: "",
-              element: <Cities />
-            }
-          ]
+          element: <Cities />
         },
       ]
     },
